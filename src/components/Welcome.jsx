607d0f8ec55cc2ca4de5db7c0b157cf850dae221
mo7hamd2003 <mohmdsteitieh123@gmail.com>
2025-12-28 21:47:14 +0200

    feat/dock

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -17,7 +17,7 @@ const renderText = (text, className, baseWeight = 400) => {
     ));
 };
 const setUpTextHover = (container, type) => {
-    if (!container) return;
+    if (!container) return () => {};
 
     const letters = container.querySelectorAll("span");
     const { min, max, default: base } = FONT_WEIGHT[type];
